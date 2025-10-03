7629ca75bde2df26664862ce39a56d72909692cb
jj2084jj <whdms3368@gmail.com>
2025-10-03 23:22:05 +0900

    add : 배경효과 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import Calendar from '@/sections/Calendar';
-import Cover from '@/sections/Cover';
-import Gallery from '@/sections/Gallery';
-import Letter from '@/sections/Letter';
-import Location from '@/sections/Location';
+import Calendar from "@/sections/Calendar";
+import Cover from "@/sections/Cover";
+import Gallery from "@/sections/Gallery";
+import Letter from "@/sections/Letter";
+import Location from "@/sections/Location";
 
 export default function Home() {
   return (
     <div
-      className='min-h-screen flex flex-col items-stretch'
-      style={{ display: 'contents' }}
+      className="min-h-screen flex flex-col items-stretch"
+      style={{ display: "contents" }}
     >
       <Cover />
       <Letter />
