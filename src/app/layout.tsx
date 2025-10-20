78126296fed5a391bd4db5daa257e47bd1914bd1
tjk1150 <tjk1150@kakao.com>
2025-10-20 14:36:34 +0900

    fix: 미사용 이미지 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import { LangProvider } from "@/lib/i18n/LangProvider";
-import type { Metadata } from "next";
+import { LangProvider } from '@/lib/i18n/LangProvider';
+import type { Metadata } from 'next';
 import {
   Cormorant_Garamond,
   Geist,
@@ -7,57 +7,57 @@ import {
   Gowun_Dodum,
   IBM_Plex_Sans_KR,
   Sacramento,
-} from "next/font/google";
-import { Suspense } from "react";
+} from 'next/font/google';
+import { Suspense } from 'react';
 
-import "./globals.css";
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 // Sacramento
 const sacramento = Sacramento({
-  subsets: ["latin"],
-  weight: "400",
-  variable: "--font-sacramento",
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-sacramento',
 });
 
 // IBM Plex Sans KR (구글 폰트)
 const ibm = IBM_Plex_Sans_KR({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  variable: "--font-kr",
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  variable: '--font-kr',
 });
 
 // Cormorant Garamond
 const cormorant = Cormorant_Garamond({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  variable: "--font-cormorant",
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  variable: '--font-cormorant',
 });
 
 const Gowun = Gowun_Dodum({
-  subsets: ["latin"],
-  weight: ["400"],
-  variable: "--font-gowun",
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-gowun',
 });
 
 export const metadata: Metadata = {
-  title: "진태 ♥ 조은 결혼식에 초대합니다.",
-  description: "2026년 02월 28일 토요일 2시 30분",
-  icons: { icon: "/streamline-stickies-color_love.ico" },
+  title: '진태 ♥ 조은 결혼식에 초대합니다.',
+  description: '2026년 02월 28일 토요일 2시 30분',
+  icons: { icon: '/streamline-stickies-color_love.ico' },
   openGraph: {
     images: {
-      url: "https://jintae-joeun-wedding-invitation.netlify.app/main.jpeg",
+      url: 'https://jintae-joeun-wedding-invitation.netlify.app/main.jpeg',
       width: 800,
       height: 600,
-      alt: "진태 & 조은 결혼식 초대 이미지",
+      alt: '진태 & 조은 결혼식 초대 이미지',
     },
   },
 };
@@ -68,7 +68,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='ko'>
       <body
         className={`${geistSans.variable} ${Gowun.variable} ${geistMono.variable} ${sacramento.variable} ${ibm.variable} ${cormorant.variable} antialiased`}
       >
--- src/sections/Gallery.tsx
+++ src/sections/Gallery.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import { galleryDimensions } from "@/constants/galleryDimensions";
-import { useLang, useT } from "@/lib/i18n/LangProvider";
-import Image from "next/image";
-import PhotoSwipeLightbox from "photoswipe/lightbox";
+import { galleryDimensions } from '@/constants/galleryDimensions';
+import { useLang, useT } from '@/lib/i18n/LangProvider';
+import Image from 'next/image';
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
 
-import React, { useEffect, useState } from "react";
-import "photoswipe/style.css";
-import { MagnifyingGlassPlusIcon } from "@heroicons/react/16/solid";
+import { MagnifyingGlassPlusIcon } from '@heroicons/react/16/solid';
+import 'photoswipe/style.css';
+import React, { useEffect, useState } from 'react';
 
 function generateUniqueRandoms(count: number, max: number) {
   const set = new Set<number>();
@@ -24,15 +24,15 @@ export default function Gallery(): React.ReactElement {
   const { isKr } = useLang();
 
   useEffect(() => {
-    setList(generateUniqueRandoms(30, 31)); // 1~28 중 중복 없이 10개
+    setList(generateUniqueRandoms(27, 30)); // 1~30 중 중복 없이 29개
   }, []);
 
   useEffect(() => {
     const lightbox = new PhotoSwipeLightbox({
-      gallery: "#gallery",
-      children: "a",
-      showHideAnimationType: "fade",
-      pswpModule: () => import("photoswipe"),
+      gallery: '#gallery',
+      children: 'a',
+      showHideAnimationType: 'fade',
+      pswpModule: () => import('photoswipe'),
     });
 
     lightbox.init();
@@ -44,41 +44,41 @@ export default function Gallery(): React.ReactElement {
   return (
     <>
       <section className={`gallery`}>
-        <div className="header">
-          <h2 data-aos="fade-up" className={`title ${isKr ? "kr" : "en"}`}>
-            {t("gallery.title")}
+        <div className='header'>
+          <h2 data-aos='fade-up' className={`title ${isKr ? 'kr' : 'en'}`}>
+            {t('gallery.title')}
           </h2>
           <p
-            data-aos="fade-up"
+            data-aos='fade-up'
             className={`sub-title flex items-center gap-1 ${
-              isKr ? "kr" : "en"
+              isKr ? 'kr' : 'en'
             }`}
           >
-            <MagnifyingGlassPlusIcon className="w-4 h-4" />
-            {t("gallery.sub_title")}
+            <MagnifyingGlassPlusIcon className='w-4 h-4' />
+            {t('gallery.sub_title')}
           </p>
         </div>
 
         <div
-          id="gallery"
-          className={`${showList ? "h-auto" : "h-[550px]"} overflow-hidden`}
+          id='gallery'
+          className={`${showList ? 'h-auto' : 'h-[550px]'} overflow-hidden`}
         >
           {list &&
             list.map((p, idx) => (
               <a
                 key={idx}
-                data-aos="fade-in"
-                className="slide aspect-auto"
+                data-aos='fade-in'
+                className='slide aspect-auto'
                 href={`/gallery/image${p}.webp`}
                 data-pswp-width={galleryDimensions[p]?.width ?? 853}
                 data-pswp-height={galleryDimensions[p]?.height ?? 1280}
-                target="_blank"
-                rel="noreferrer"
+                target='_blank'
+                rel='noreferrer'
               >
                 <Image
-                  className="thumbnail"
+                  className='thumbnail'
                   src={`/gallery/image${p}.webp`}
-                  alt=""
+                  alt=''
                   width={galleryDimensions[p]?.width ?? 853}
                   height={galleryDimensions[p]?.height ?? 1280}
                 />
@@ -90,15 +90,15 @@ export default function Gallery(): React.ReactElement {
           onClick={() => {
             setShowList(!showList);
           }}
-          className="!bg-[#e8d8cd] !mt-4 !py-4 !w-full !text-[#806867] m-auto text-center !rounded-2xl"
+          className='!bg-[#e8d8cd] !mt-4 !py-4 !w-full !text-[#806867] m-auto text-center !rounded-2xl'
         >
-          {showList ? "접기" : "더보기"}
+          {showList ? '접기' : '더보기'}
         </button>
       </section>
       <img
-        src="/assets/gallery-bottom-wave.svg"
-        className="gallery-bottom-wave"
-        alt=""
+        src='/assets/gallery-bottom-wave.svg'
+        className='gallery-bottom-wave'
+        alt=''
       />
     </>
   );
